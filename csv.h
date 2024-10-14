1603fda049bb380dbb0104937c5ea7345bbab897
DerzhiZhabu <94835516+DerzhiZhabu@users.noreply.github.com>
2024-10-14 04:16:43 -0400

    Working project

---
+++ csv.h
@@ -0,0 +1,7 @@
+#include <string>
+#include <filesystem>
+
+void unlock(const filesystem::path& directory, const std::string tableName);
+void writeToCsv(const filesystem::path& filename, List<List<string>>& data, string table_name, string& schem_name);
+void readFromCsv(const filesystem::path& filename, List<List<string>>& data, string table_name, string& schem_name);
+void appendToCsv(const filesystem::path& filename, List<string>& newRow, string table_name, string& schem_name);
---
+++ json_parse.h
@@ -0,0 +1,5 @@
+#include <string>
+
+using namespace std;
+
+void getJSON(HashTable<List<string>>& tables, List<string>& tables_names, string& schem_name, int& limit);
